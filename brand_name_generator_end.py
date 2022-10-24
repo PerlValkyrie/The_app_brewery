89da74b0c8dba8ccc19868273d127bd35a465a3c
Randy Jenkins <jenkins-island@hotmail.com>
2022-10-24 15:21:39 -0500

    first commit

---
+++ brand_name_generator_end.py
@@ -0,0 +1,11 @@
+welcome_message = "Welcome ot the Band Name Generator"
+
+city_question = input("What's name of the city you grew up in? ")
+
+pet_question = input("What's your pet's name? ")
+
+response = "Your band name could be " + " " + city_question + " " + pet_question + "."
+
+print(response)
+
+input("\n\nPress enter to end the program.")

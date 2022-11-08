e352f537b859fb7014051d5db02f997e85148538
Randy Jenkins <jenkins-island@hotmail.com>
2022-11-08 06:17:52 -0600

    Added small programs from Absolute beginner book.

---
+++ Python_AbsoluteBeginner_Book/Password_program.py
@@ -0,0 +1,15 @@
+# Highly secure password program
+
+print("Welcome to System Security Inc.")
+
+print("- - where security is our middle name\n")
+
+password = input("Enter your password: ")
+
+if password == "secret":
+    print("Access Granted")
+if password != "secret":
+    print("Access Denied")
+    
+
+input("\n\nPress the enter key to exit.")
